77d8ad0fb86e7c7d16d6bd14d71249c7ec180cb5
Naveen Gali <naveengali80@gmail.com>
2023-09-25 11:03:54 +0530

    Mongoose Settedup.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("INDEX");
+const express = require("express");
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL).then(
+  () => {
+    console.log("DB Connected!");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
