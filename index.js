bb77813fc095b66e06965cf4d819339d1f8b3c81
Naveen Gali <naveengali80@gmail.com>
2023-09-28 16:41:27 +0530

    Reverting Mistaked Commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("INDEX");
+const express = require("express");
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRouter = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+
+mongoose.connect(process.env.MONGO_URL).then(
+  () => {
+    console.log("DB Connected!");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server Started");
+});
---
+++ models/MenuItem.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    category: String,
+    restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
