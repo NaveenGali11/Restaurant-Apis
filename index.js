17812ca0e4bd3bdce755e524a5971beaac77253e
Naveen Gali <naveengali80@gmail.com>
2023-09-28 16:22:10 +0530

    Menu Addition, Updation, Get all menus for restaurant, Delete Menu Added.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const restaurantRouter = require("./routes/restaurant");
+const menuRouter = require("./routes/menuItem");
 
 dotenv.config();
 
@@ -17,17 +18,19 @@ app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
+app.use("/api/menu", menuRouter);
 app.use("/api/restaurants", restaurantRouter);
 
+
 mongoose.connect(process.env.MONGO_URL).then(
-  () => {
-    console.log("DB Connected!");
-  },
-  (err) => {
-    console.log(err);
-  }
+    () => {
+        console.log("DB Connected!");
+    },
+    (err) => {
+        console.log(err);
+    }
 );
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server Started");
+    console.log("Server Started");
 });
